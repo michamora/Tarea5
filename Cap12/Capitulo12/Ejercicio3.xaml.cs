ada2d9070ba7c32e3424afe49521e8dfce212041
micha mora <michamora4@gmail.com>
2022-02-16 16:35:32 -0400

    Ejercicio 3

--- Cap12/Capitulo12/Ejercicio3.xaml.cs
+++ Cap12/Capitulo12/Ejercicio3.xaml.cs
@@ -2,13 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
-
+      
 namespace Cap12.Capitulo12
 {
     /// <summary>
     /// Interaction logic for Ejercicio1.xaml
     /// </summary>
-
+    
     /*
         Ejercicio 3 : Buscar en MSDN cuales son las excepciones que pueden ocurrir con los arreglos.
     */
